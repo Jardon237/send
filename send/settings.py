48f335771d8c0759c3b8ed28be29f2f6eb9efe7c
̂̂̂̂jardon237̂ <ngwajude161@gmail.com>
2023-05-05 11:55:51 +0100

    updated templates

--- send/settings.py
+++ send/settings.py
@@ -31,17 +31,35 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'send_me',
-    'django.contrib.admin',
-    'django.contrib.auth',
+
+
+SHARED_APPS = (
+    'django_tenants',  # mandatory
+    'customers', # you must list the app where your tenant model resides in
+
     'django.contrib.contenttypes',
+
+    # everything below here is optional
+    'django.contrib.auth',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+    'django.contrib.admin',
+)
+
+TENANT_APPS = (
+    # your tenant-specific apps
+    'send_me',
+    
+)
+
+INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in SHARED_APPS]
+TENANT_MODEL = "customers.Client" # app.Model
+
+TENANT_DOMAIN_MODEL = "customers.Domain"  # app.Model
 
 MIDDLEWARE = [
+    'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_tenants.middleware.TenantSubfolderMiddleware',
 ]
 
 ROOT_URLCONF = 'send.urls'
@@ -62,6 +81,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                #'django.template.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
@@ -79,11 +99,19 @@ WSGI_APPLICATION = 'send.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_tenants.postgresql_backend',
+        'NAME': 'SEND_ME2',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
+#data base routers
+DATABASE_ROUTERS = (
+    'django_tenants.routers.TenantSyncRouter',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,18 +148,34 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
-STATIC_ROOT = (BASE_DIR/'asert/')
+
 
 JANGO_ICONS = {
     "ICONS": {
         "edit": {"name": "far fa-pencil"},
     },
 }
+TENANT_SUBFOLDER_PREFIX = "clients"
+
+
+
+
+'''
+admin=Client(schema_name='public', company='hooyia', paid_until='2000-10-03', on_trial=True)
+>>> admin.save()
+>>> domain=Domain()
+>>> domain.domain='localhost'
+>>> domain.tenant=admin
+>>> domain.is_primary=True
+>>> domain.save()
+>>> 
+'''
